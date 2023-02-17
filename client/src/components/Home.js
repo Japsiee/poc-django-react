3aa1c6661a43fa2d8f2f8d1a6a75bdd90223b180
jp japos <dyollsopaj@gmail.com>
2023-02-17 23:32:37 +0800

    [fix] remove other stream options

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -52,7 +52,6 @@ const Home = () => {
         className="flex justify-center items-center bg-gray-600 w-4/6 h-full relative mx-auto text-white"
       >
         <div className="absolute w-full top-0 left-0 h-full">
-          {/* 1st option -> normal video stream */}
           <video
             id="stream-viewer"
             className="absolute w-full top-0 left-0 h-full"
@@ -60,23 +59,10 @@ const Home = () => {
             autoPlay
           >
             <source
-              src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4"
-              type='video/mp4; codecs="avc1.42E01E, mp4a.40.2" '
+              src="http://localhost:8080/hls/stream.m3u8"
+              type="application/x-mpegURL"
             />
           </video>
-
-          {/* 2nd option -> this one is for http streaming */}
-          {/* <video
-            id="stream-viewer"
-            className="absolute w-full top-0 left-0 h-full"
-            controls
-            autoPlay
-          >
-            <source src="http://localhost:8080/hls/stream.m3u8" type="application/x-mpegURL" />
-          </video> */}
-
-          {/* 3rd option -> iframe http stream */}
-          {/* <iframe className="absolute top-0 left-0 w-full h-full" src="https://test.antmedia.io:5080/WebRTCAppEE/play.html?name=554221610006205395756180" frameborder="0" allowfullscreen></iframe> */}
         </div>
         <div id="camera-pan" className="flex flex-col absolute top-10 right-10">
           <AiOutlinePlusCircle className="text-6xl my-2" />
